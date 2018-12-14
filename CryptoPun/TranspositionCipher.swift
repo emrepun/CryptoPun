4ce3c937fe8f4f57cca8635393b8ca884099da60
emrepun <emrehavan@hotmail.com>
2018-12-14 13:08:07 +0000

    Initial commit

---
+++ CryptoPun/TranspositionCipher.swift
@@ -0,0 +1,19 @@
+//
+//  TranspositionCipher.swift
+//  CryptoPun
+//
+//  Created by Emre HAVAN on 14.12.2018.
+//  Copyright © 2018 Emre HAVAN. All rights reserved.
+//
+
+import Foundation
+
+class TranspositionCipher {
+    private init() {}
+    
+    let shared = TranspositionCipher()
+    
+    private func decode(message: String, period: Int, permutation: (Int, Int)) {
+        
+    }
+}
---
+++ CryptoPun/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CryptoPun
+//
+//  Created by Emre HAVAN on 14.12.2018.
+//  Copyright © 2018 Emre HAVAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
